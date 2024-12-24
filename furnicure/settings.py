396d9fc1caa2f7d47d4767841d8a13807e07ddaf
therajesh1 <therajeshchheda@gmail.com>
2024-12-24 15:19:51 +0530

    A

--- furnicure/settings.py
+++ furnicure/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'furnicure.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fcure',
+        'USER': 'root',
+        'PASSWORD': '123abcd123',
+        'HOST': '127.0.0.1',  # Or your database server's IP address
+        'PORT': '3306',       # Default MySQL port
     }
 }
 
