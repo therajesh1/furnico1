35cc70a6fd9e405514c2efec2853ba31b56950ea
therajesh1 <therajeshchheda@gmail.com>
2024-12-24 13:45:20 +0530

    A

--- eco/admin.py
+++ eco/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 # Register your models here.
 # your_app/admin.py
 from django.contrib import admin
-from .models import Category,Order,Product,Customer
+from .models import Category,Order,Product,Customer,Shopkeeper
 
 admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(Product)
 admin.site.register(Customer)
+admin.site.register(Shopkeeper)
 
