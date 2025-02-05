888256dab6db9101f19688b945e9ecff774a3f9d
therajesh1 <therajeshchheda@gmail.com>
2025-02-05 21:35:25 +0530

    A

--- eco/admin.py
+++ eco/admin.py
@@ -11,5 +11,10 @@ admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Shopkeeper)
 admin.site.register(Contact)
-admin.site.register(InternshipApplication)
 
+class InternshipApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'college', 'year', 'role')
+    search_fields = ('name', 'email', 'college', 'role')
+    list_filter = ('year', 'role')
+    
+admin.site.register(InternshipApplication, InternshipApplicationAdmin)
