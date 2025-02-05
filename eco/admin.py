46997785ffc3f30f4ccdb1dbd15ed4cc04f3b7a3
therajesh1 <therajeshchheda@gmail.com>
2025-02-05 21:49:35 +0530

    A

--- eco/admin.py
+++ eco/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
-# your_app/admin.py
-from django.contrib import admin
-from .models import Category,Order,Product,Customer,Shopkeeper,Contact,InternshipApplication
+from .models import Category, Order, Product, Customer, Shopkeeper, Contact, InternshipApplication
 
 admin.site.register(Category)
 admin.site.register(Order)
@@ -13,8 +9,10 @@ admin.site.register(Shopkeeper)
 admin.site.register(Contact)
 
 class InternshipApplicationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'college', 'year', 'role')
+    list_display = ('name', 'email', 'college', 'year', 'role', 'applied_at')  # Fixed field name
     search_fields = ('name', 'email', 'college', 'role')
     list_filter = ('year', 'role')
-    
+    readonly_fields = ('applied_at',)  # Fixed field name
+
 admin.site.register(InternshipApplication, InternshipApplicationAdmin)
+
